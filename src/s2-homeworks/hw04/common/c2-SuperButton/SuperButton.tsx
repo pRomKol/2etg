c391c57ff7e7639ba4036a7148faf596071b1002
RomanKolosov <roman_kolosov@list.ru>
2024-01-02 13:30:52 +0300

    123

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -21,7 +21,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         + (disabled
                 ? s.disabled
                  : xType === 'red'
-                     ? s.red : '')
+                     ? s.default : '')
         + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
