6e2a298466a535cd4e8399c66e1ca9861582fcd3
RomanKolosov <roman_kolosov@list.ru>
2024-01-09 19:40:02 +0300

    123

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -18,7 +18,7 @@ function Pages() {
             <Routes>
                 роутинг будут писать студенты
                 в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior
-                {/*<Route path={'/'}/>*/}
+                <Route path={'/'} element={<PreJunior/>}/>
 
                 роуты для /pre-junior, /junior, /junior-plus
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
@@ -26,7 +26,7 @@ function Pages() {
                 <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
 
                {/* роут для несуществующей страницы должен отрисовать <Error404 />*/}
-               <Route path={'/'} element={<Error404/>}/>
+               <Route path={'*'} element={<Error404/>}/>
             </Routes >
         </div>
     )
