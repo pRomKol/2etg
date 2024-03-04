47097263244aabfff7680f25cdd74bb6c325e366
Roman <sselbstmord@gmail.com>
2024-03-04 13:59:30 +0300

    This never done(

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -44,7 +44,7 @@ function Clock() {
     const normalizeTime = (timeEl: number) => {
         return timeEl < 10? '0'+ timeEl: timeEl
     }
-    const stringTime =  normalizeTime(date.getHours()) + ":" + normalizeTime(date.getMinutes())|| <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringTime =  normalizeTime(date.getHours()) + ":" + normalizeTime(date.getMinutes()) + ':' + normalizeTime(date.getSeconds()) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = normalizeTime(date.getDate()) + '.' + normalizeTime(date.getMonth()+1) + '.' + normalizeTime(date.getFullYear()) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
